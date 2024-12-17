9de44140fc848d60db59f032125a2f25dfece8b9
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-12-17 18:08:48 +0530

    Using Socket.io with Express

--- server.js
+++ server.js
@@ -1,39 +1,14 @@
-const server = require('http').createServer();
-const io = require('socket.io')(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-  },
-});
-const PORT = 3000;
+const http = require('http');
+const io = require('socket.io');
 
-server.listen(PORT);
+const apiServer = require('./api');
+const httpServer = http.createServer(apiServer);
+const socketServer = io(httpServer);
+
+const sockets = require('./sockets');
+
+const PORT = 3000;
+httpServer.listen(PORT);
 console.log(`Listening on port ${PORT}...`);
 
-let readyPlayerCount = 0;
-
-io.on('connection', socket => {
-  console.log('a user connected', socket.id);
-
-  socket.on('ready', () => {
-    console.log('Player ready', socket.id);
-
-    readyPlayerCount++;
-
-    if (readyPlayerCount % 2 === 0) {
-      io.emit('startGame', socket.id);
-    }
-  });
-
-  socket.on('paddleMove', paddleData => {
-    socket.broadcast.emit('paddleMove', paddleData);
-  });
-
-  socket.on('ballMove', ballData => {
-    socket.broadcast.emit('ballMove', ballData);
-  });
-
-  socket.on('disconnect', reason => {
-    console.log(`Client ${socket.id} disconnected: ${reason}`);
-  });
-});
+sockets.listen(socketServer);
